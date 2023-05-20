d4b2e8defeb2dfd8c4596022779ad2818fa9f6e4
mmmsoup <benjecock@tutanota.com>
2023-05-20 19:47:31 +0100

    change name to parkstats, add parkrun bingo shenanigans

--- loader.js
+++ loader.js
@@ -1,17 +1,17 @@
 // load maplibregl and then main content script
 
-function load_parkmapper_js() {
-	const parkmapper_js = document.createElement("script");
-	parkmapper_js.type = "text/javascript";
-	parkmapper_js.src = browser.runtime.getURL("parkmapper.js");
-	document.head.appendChild(parkmapper_js);
+function load_parkstats_js() {
+	const parkstats_js = document.createElement("script");
+	parkstats_js.type = "text/javascript";
+	parkstats_js.src = browser.runtime.getURL("parkstats.js");
+	document.head.appendChild(parkstats_js);
 }
 
 function load_maplibregl_css() {
 	const maplibregl_css = document.createElement("link");
 	maplibregl_css.rel = "stylesheet";
 	maplibregl_css.href = "https://unpkg.com/maplibre-gl@2.4.0/dist/maplibre-gl.css";
-	maplibregl_css.onload = load_parkmapper_js;
+	maplibregl_css.onload = load_parkstats_js;
 	document.head.appendChild(maplibregl_css);
 }
 
